ff9f78e06b934fcb6072d8944e72222ec08cd9a4
Vũ Quang Nguyên <nguyenvqse180208@fpt.edu.vn>
2025-09-05 21:05:52 +0700

    Add project files.

---
+++ BaseResponse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class BaseResponse
+    {
+        public bool Success { get; set; } = true;
+        public string Message { get; set; } = "OK";
+        public List<string>? Errors { get; set; }
+
+        public static BaseResponse Ok(string message = "Success")
+            => new() { Success = true, Message = message };
+
+        public static BaseResponse Fail(string message, List<string>? errors = null)
+            => new() { Success = false, Message = message, Errors = errors };
+    }
+
+    public class BaseResponse<T> : BaseResponse
+    {
+        public T? Data { get; set; }
+
+        public static BaseResponse<T> Ok(T data, string message = "Success")
+            => new() { Success = true, Message = message, Data = data };
+
+        public static new BaseResponse<T> Fail(string message, List<string>? errors = null)
+            => new() { Success = false, Message = message, Errors = errors };
+    }
+}
---
+++ Paging.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class PagedResponse<T> : BaseResponse<List<T>>
+    {
+        public int TotalCount { get; set; }
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+
+        public static PagedResponse<T> Ok(List<T> data, int total, int page, int pageSize, string message = "Success")
+        {
+            return new PagedResponse<T>
+            {
+                Success = true,
+                Message = message,
+                Data = data,
+                TotalCount = total,
+                Page = page,
+                PageSize = pageSize
+            };
+        }
+
+        public static PagedResponse<T> Fail(string message, int page, int pageSize)
+        {
+            return new PagedResponse<T>
+            {
+                Success = false,
+                Message = message,
+                Data = new List<T>(),
+                TotalCount = 0,
+                Page = page,
+                PageSize = pageSize
+            };
+        }
+    }
+}
