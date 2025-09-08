2976580ff29697683ab3aa0abe3050b38d20b9d5
Vũ Quang Nguyên <nguyenvqse180208@fpt.edu.vn>
2025-09-08 07:20:18 +0700

    Refactor project structure and update namespaces
    - Added new folder structure in `AuthProject.Domain.csproj` for `Entities` and `Enum`.
    - Moved `User` class to `Common.Entities` namespace and updated references.
    - Added reference to `Common.csproj` in `AuthProject.Infrastructure.csproj`.
    - Removed outdated using directives related to `AuthProject.Domain.Entities`.
    - Relocated `ExceptionHandlingMiddleware` to `Common.Middlewares` namespace.
    - Overall, these changes improve code organization and maintainability.

--- Middlewares/ExceptionHandlingMiddleware.cs
+++ Middlewares/ExceptionHandlingMiddleware.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using System.Text.Json;
 
-namespace AuthProject.WebApi.Middlewares
+namespace Common.Middlewares
 {
     public class ExceptionHandlingMiddleware
     {
