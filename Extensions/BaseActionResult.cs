056c9efb2b5150c02dba9034450d18ead257a336
Vũ Quang Nguyên <nguyenvqse180208@fpt.edu.vn>
2025-09-15 18:52:54 +0700

    Refactor data access and add custom action results
    Replaced `AuthProject.Infrastructure.Data` with `Common.Data` across multiple files to reorganize the data access layer. Introduced new classes `BaseActionResult<TData>` and `ControllerBaseWithBaseReponse` to provide structured action results with metadata in ASP.NET Core. Added a package reference for `Microsoft.AspNetCore.Mvc.Core` in `Common.csproj` to support the new functionality.

---
+++ Extensions/BaseActionResult.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace PRN232.Lab1.CoffeeStore.API.Extensions
+{
+    public class BaseActionResult<TData> : IConvertToActionResult
+    {
+        private const int DefaultStatusCode = StatusCodes.Status200OK;
+
+        public bool? Success { get; set; }
+
+        public string? Message { get; set; }
+
+        public TData? Data { get; }
+
+        public ActionResult? Result;
+
+        public BaseActionResult(TData data, string? message = "Message", bool? success = true)
+        {
+            Data = data;
+            Message = message;  
+            Success = success;
+        }
+
+        public BaseActionResult(ActionResult result, string? message = "Message", bool? success = true)
+        {
+            Result = result;
+            Message = message;
+            Success = success;
+        }
+
+        public static implicit operator BaseActionResult<TData>(TData value) => new(value);
+
+        public static implicit operator BaseActionResult<TData>(ActionResult result) => new(result);
+
+        public IActionResult Convert()
+        {
+            if (Result != null)
+            {
+                return Result;
+            }
+
+            int statusCode;
+            if (Data is ProblemDetails problemDetails && problemDetails.Status != null)
+            {
+                statusCode = problemDetails.Status.Value;
+            }
+            else
+            {
+                statusCode = DefaultStatusCode;
+            }
+
+            return new ObjectResult(Data)
+            {
+                DeclaredType = typeof(TData),
+                StatusCode = statusCode
+            };
+        }
+    }
+}
---
+++ Extensions/ControllerBaseWithBaseReponse.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PRN232.Lab1.CofeeStore.API.Extensions
+{
+    public class ControllerBaseWithBaseReponse : ControllerBase
+    {
+        [NonAction]
+        public ActionResult Ok(string? message = "Success")
+        {
+            return base.Ok(new { Success = true, Message = message });
+        }
+
+        [NonAction]
+        public ActionResult Ok(object data, string? message = "Success")
+        {
+            return base.Ok(new { Success = true, Message = message, Data = data });
+        }
+
+        [NonAction]
+        public ActionResult BadRequest(string? message = "Bad Request")
+        {
+            return base.BadRequest(new { Success = false, Message = message });
+        }
+
+        [NonAction]
+        public ActionResult BadRequest(object data, string? message = "Bad Request")
+        {
+            return base.BadRequest(new { Success = false, Message = message, Data = data });
+        }
+
+        [NonAction]
+        public ActionResult NotFound(string? message = "Not Found")
+        {
+            return base.NotFound(new { Success = false, Message = message });
+        }
+
+        [NonAction]
+        public ActionResult NotFound(object data, string? message = "Not Found")
+        {
+            return base.NotFound(new { Success = false, Message = message, Data = data });
+        }
+    }
+}
