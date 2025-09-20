d954f7732a7d53ba220817783c1d79621ec0b127
Vũ Quang Nguyên <nguyenvqse180208@fpt.edu.vn>
2025-09-20 22:53:16 +0700

    Add BaseDependencyInjection for DbContext registration
    - Introduced `BaseDependencyInjection` class in `Common.Extensions` with `AddBaseServicesWithDbContext<TDbContext>` method for simplified DbContext registration.
    - Replaced old database context registration in `DependencyInjection.cs` with the new method to enhance code reusability.
    - Updated `Common.csproj` to include `Microsoft.EntityFrameworkCore.SqlServer` and modified version of `Microsoft.EntityFrameworkCore`.
    - Removed reference to `Common` package in `UserProject.WebApi.csproj`.
    - Added necessary using directives in `BaseDependencyInjection.cs` and `DependencyInjection.cs`.
    - Reformatted XML in project files for consistency.

---
+++ Extensions/BaseDependencyInjection.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Extensions
+{
+    public static class BaseDependencyInjection
+    {
+        public static IServiceCollection AddBaseServicesWithDbContext<TDbContext>(this IServiceCollection services, IConfiguration configuration)
+            where TDbContext : DbContext
+        {
+            var connectionString = configuration["DATABASE_CONNECTION_STRING"];
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                Console.WriteLine("Warning: DATABASE_CONNECTION_STRING is not set in environment variables.");
+            }
+
+            services.AddDbContext<TDbContext>(options =>
+                options.UseSqlServer(connectionString)
+                       .ConfigureWarnings(w => w.Ignore(RelationalEventId.PendingModelChangesWarning)));
+
+            return services;
+        }
+    }
+
+}
