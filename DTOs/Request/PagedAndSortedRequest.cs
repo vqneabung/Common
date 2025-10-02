63d1175d8f930f4c96a80cb9ff3e5123310a3d9d
Vũ Quang Nguyên <nguyenvqse180208@fpt.edu.vn>
2025-10-02 23:49:47 +0700

    Refactor services and update entity identifiers to GUIDs
    - Transitioned entity identifiers from `int` to `Guid` for consistency and scalability across services.
    - Added `PagedAndSortedRequest` for pagination and sorting in `GetAll` methods.
    - Implemented missing service methods for `AuthService`, `CategoryService`, `OrderService`, `PaymentService`, `ProductService`, and `UserService`.
    - Updated `IUnitOfWork` to remove `Users` and `Auths` repositories.
    - Introduced dependency injection for `UserManager<User>` and `IMapper` in `AuthService`.
    - Added folder `DTOs\Response` to organize response DTOs.
    - Improved maintainability and alignment with modern development practices.

---
+++ DTOs/Request/PagedAndSortedRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Common.DTOs.Request
+{
+	public class PagedAndSortedRequest
+	{
+		public int PageNumber { get; set; } = 1;
+		public int PageSize { get; set; } = 10;
+
+		public string? SortBy { get; set; }
+		public bool SortDescending { get; set; } = false;
+	}
+}
