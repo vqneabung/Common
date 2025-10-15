3d45e530ea7602a5d4eb692d29cf795c20193753
Vũ Quang Nguyên <nguyenvqse180208@fpt.edu.vn>
2025-10-15 16:42:31 +0700

    Refactor API responses and update data models
    - Updated `Common` submodule to a new commit with local changes.
    - Changed `Login` method in `AuthController` to return `Unauthorized` for errors.
    - Expanded `Authorize` roles in `OrdersController` for `GetOrderDetailById`.
    - Updated `CategoryId` in `CommonProductRequest` to use `Guid?`.
    - Refactored `OrderDetailResponse` and `ProductResponse` to use `Guid` types.
    - Replaced `_mapper.Map<User>` with manual mapping in `AuthService`.
    - Added `Unauthorized` helper methods in `ControllerBaseWithBaseResponse`.

--- Extensions/ControllerBaseWithBaseReponse.cs
+++ Extensions/ControllerBaseWithBaseReponse.cs
@@ -39,5 +39,19 @@ namespace Common.Extensions
         {
             return base.NotFound(new { Success = false, Message = message, Data = data });
         }
+
+        [NonAction]
+        public ActionResult Unauthorized(string? message = "Not Found")
+        {
+            // No overload for Unauthorized takes an object, so use StatusCode with 401
+            return StatusCode(401, new { Success = false, Message = message });
+        }
+
+        [NonAction]
+        public ActionResult Unauthorized(object data, string? message = "Not Found")
+        {
+            // No overload for Unauthorized takes an object, so use StatusCode with 401
+            return StatusCode(401, new { Success = false, Message = message, Data = data });
+        }
     }
 }
