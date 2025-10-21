279d2cd3dcc9e1fbaa56096810db8f1329112ee7
Vũ Quang Nguyên <nguyenvqse180208@fpt.edu.vn>
2025-10-21 22:41:29 +0700

    Enhance validation, authorization, and query handling
    - Updated subproject commit to include `-dirty` suffix.
    - Refactored `CategoriesController` to move `[Authorize]` to methods.
      Added `Create` and `Update` methods for category management.
    - Fixed query parameter binding in `OrdersController`'s `GetPagedAsync`.
    - Added validation attributes (`[Required]`, `[EmailAddress]`, `[Range]`)
      to `Login`, `RegisterRequest`, and `PagedAndSortedRequest` models.
    - Adjusted namespaces and `using` directives for consistency.

--- DTOs/Request/PagedAndSortedRequest.cs
+++ DTOs/Request/PagedAndSortedRequest.cs
@@ -1,11 +1,16 @@
-﻿namespace Common.DTOs.Request
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Common.DTOs.Request
 {
 	public class PagedAndSortedRequest
 	{
+		[Range(0, int.MaxValue, ErrorMessage = "PageNumber must be greater than 0")]
 		public int PageNumber { get; set; } = 1;
+
+        [Range(0, int.MaxValue, ErrorMessage = "PageSize must be greater than 0")]
 		public int PageSize { get; set; } = 10;
 
-		public string? SortBy { get; set; }
+        public string? SortBy { get; set; }
 		public bool SortDescending { get; set; } = false;
 	}
 }
